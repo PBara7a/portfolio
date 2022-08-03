2ca84b8a65f58966ce1d387e3e14911c2ca74f90
Paulo Barata <pbara7a@gmail.com>
2022-08-03 19:17:06 +0100

    Projects carousel.

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,6 +1,6 @@
 import { Card, Button } from "react-bootstrap";
 
-const ProjectCard = ({ imgPreview, title, description, github, demo }) => {
+const ProjectCard = ({ imgPreview, title, description, github, server }) => {
   const btnCustom = {
     display: "inline-block",
     color: "#000",
@@ -11,33 +11,48 @@ const ProjectCard = ({ imgPreview, title, description, github, demo }) => {
   };
 
   return (
-    <Card className="projects__card" style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={imgPreview} />
-      <Card.Body>
-        <Card.Title style={{ color: "#ccff00" }}>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Button
-          href={github}
-          target="_blank"
-          style={btnCustom}
-          variant="primary"
-          className="btn-custom"
-        >
-          GitHub
-        </Button>
-        {demo && (
-          <Button
-            href={demo}
-            target="_blank"
-            style={btnCustom}
-            variant="primary"
-            className="btn-custom"
-          >
-            Demo
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
+    <div className="card-frame">
+      <Card className="projects__card" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={imgPreview} />
+        <Card.Body>
+          <Card.Title style={{ color: "#ccff00" }}>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          {!server && (
+            <Button
+              href={github}
+              target="_blank"
+              style={btnCustom}
+              variant="primary"
+              className="btn-custom"
+            >
+              GitHub
+            </Button>
+          )}
+          {server && (
+            <>
+              <Button
+                href={github}
+                target="_blank"
+                style={btnCustom}
+                variant="primary"
+                className="btn-custom"
+              >
+                Client
+              </Button>
+              <Button
+                href={server}
+                target="_blank"
+                style={btnCustom}
+                variant="primary"
+                className="btn-custom"
+              >
+                Server
+              </Button>
+            </>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,5 +1,6 @@
-import { Container } from "react-bootstrap";
-import rustychainPreview from "../../assets/rustychain-preview.png";
+import { Carousel, Container } from "react-bootstrap";
+import rustychainPreview from "../../assets/rustychain.jpg";
+import chatAppPreview from "../../assets/chat.jpg";
 import ProjectCard from "./ProjectCard";
 import "./projects.css";
 
@@ -11,14 +12,26 @@ const Projects = () => {
       </h1>
 
       <Container className="d-flex justify-content-center">
-        <ProjectCard
-          imgPreview={rustychainPreview}
-          title="Rustychain"
-          description="Full stack blockchain application. Transactions are validated
-                  and recorded on the chain. Frontend: React. Backend: Node, Express, PubNub."
-          github="https://github.com/PBara7a/rustychain"
-          demo="https://calm-harbor-77723.herokuapp.com/"
-        />
+        <Carousel>
+          <Carousel.Item interval={5000}>
+            <ProjectCard
+              imgPreview={rustychainPreview}
+              title="Rustychain"
+              description="Full stack blockchain application. Transactions are validated
+                  and recorded on the chain. Built with: React, Node.js, Express, PubNub."
+              github="https://github.com/PBara7a/rustychain"
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={5000}>
+            <ProjectCard
+              imgPreview={chatAppPreview}
+              title="Chat In The Box"
+              description="A real-time messaging app with end-to-end encryption. Built with: React, Bootstrap, Node.js, Express, Socket.io, Prisma and PostgreSQL."
+              github="https://github.com/PBara7a/chat-app-client"
+              server="https://github.com/PBara7a/chat-app-server"
+            />
+          </Carousel.Item>
+        </Carousel>
       </Container>
     </Container>
   );
