43e006f1ec2a6af456e948ed6cc44e2bb82453ac
Paulo Barata <pbara7a@gmail.com>
2022-08-03 17:54:59 +0100

    Footer responsiveness.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,55 +1,45 @@
-import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { BsInstagram, BsLinkedin, BsGithub } from "react-icons/bs";
 import "./footer.css";
 
 export default function Footer() {
   const year = new Date().getFullYear();
 
-  const iconStyles = {
-    color: "white",
-    background: "transparent",
-    width: "auto",
-    borderStyle: "none",
-  };
-
   return (
     <footer>
       <Container fluid>
         <Row>
           <Col md />
-          <Col>
+          <Col className="copyright-container">
             <span className="copyright">Copyright © {year}</span>
           </Col>
           <Col>
-            <ListGroup horizontal>
-              <ListGroup.Item
-                style={iconStyles}
+            <div className="socials-container">
+              <div
                 variant="dark"
                 action
                 href="https://github.com/pbara7a"
                 target="_blank"
               >
                 <BsGithub />
-              </ListGroup.Item>
+              </div>
 
-              <ListGroup.Item
-                style={iconStyles}
+              <div
                 action
                 href="https://www.linkedin.com/in/paulo-barata/"
                 target="_blank"
               >
                 <BsLinkedin />
-              </ListGroup.Item>
+              </div>
 
-              <ListGroup.Item
-                style={iconStyles}
+              <div
                 action
                 href="https://www.instagram.com/p_bara7a/"
                 target="_blank"
               >
                 <BsInstagram />
-              </ListGroup.Item>
-            </ListGroup>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
