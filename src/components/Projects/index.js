e56d206d2d6ee48f388c51db571a4b389b1e27a3
Paulo Barata <pbara7a@gmail.com>
2022-09-02 13:05:31 +0100

    Added new project and updated cv.

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,5 @@
 import { Carousel, Container } from "react-bootstrap";
+import workoutPreview from "../../assets/workout.jpg";
 import rustychainPreview from "../../assets/rustychain.jpg";
 import chatAppPreview from "../../assets/chat.jpg";
 import ProjectCard from "./ProjectCard";
@@ -15,6 +16,16 @@ const Projects = () => {
         <Carousel>
           <Carousel.Item interval={5000}>
             <ProjectCard
+              imgPreview={workoutPreview}
+              title="Workout Planner"
+              description="A SPA I built to plan and save my workouts. Built with: React, Node.js, Express, Prisma and PostgreSQL."
+              github="https://github.com/PBara7a/workout-planner-client"
+              server="https://github.com/PBara7a/workout-planner"
+            />
+          </Carousel.Item>
+
+          <Carousel.Item interval={5000}>
+            <ProjectCard
               imgPreview={chatAppPreview}
               title="Chat In The Box"
               description="A real-time messaging app with end-to-end encryption. Built with: React, Bootstrap, Node.js, Express, Socket.io, Prisma and PostgreSQL."
@@ -22,6 +33,7 @@ const Projects = () => {
               server="https://github.com/PBara7a/chat-app-server"
             />
           </Carousel.Item>
+
           <Carousel.Item interval={5000}>
             <ProjectCard
               imgPreview={rustychainPreview}
